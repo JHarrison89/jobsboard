b3b44f04b70cac0f40cdfa7dc12192f902c1ff27
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-05-01 20:36:40 +0100

    adding festicket

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,15 +12,15 @@ require 'watir'
 require 'webdrivers/chromedriver'
 require 'byebug'
 
-
 puts "Running seed..."
-
 puts "Destroying Job table..."
 Job.delete_all
 
+puts "Running Doors Open..."
 browser = Watir::Browser.new
 browser.goto 'https://www.doorsopen.co/jobs/?q=uk'
 
+puts "Clicking load more..."
 while browser.button(text: 'Load more').present?
   browser.button(text: 'Load more').click
   sleep 1
@@ -35,12 +35,45 @@ job_listings.each do |job_listing|
     employer: job_listing.css('.listing-item__info--item-company').text.strip,
     location: job_listing.css('.listing-item__info--item-location').text.strip,
     short_desc: job_listing.css('.listing-item__desc').text,
+    long_desc: Nokogiri::HTML(HTTParty.get(job_listing.css('.media-body').css('a')[0].attributes['href'].value).body).css('.container').css('.details-body__content').text,
     link: job_listing.css('.media-body').css('a')[0].attributes['href'].value,
-    long_desc: Nokogiri::HTML(HTTParty.get(job_listing.css('.media-body').css('a')[0].attributes['href'].value)).css('.container').css('.details-body__content').text
     source: "Doors Open"
   )
 end
 
 
+puts "Running Festicket..."
+browser = Watir::Browser.new
+browser.goto 'https://apply.workable.com/festicket/?lng=en'
+
+puts "Clicking show more..."
+while browser.button(text: 'Show more').present?
+  browser.button(text: 'Show more').click
+  sleep 1
+end
+
+puts "Parsing the page..."
+parse_page = Nokogiri::HTML(browser.html)
+job_listings = parse_page.css('.careers-jobs-list-styles__job--3Cb9F') # 36
+
+job_listings.each do |job_listing|
+
+  puts "Opening jobs page and copying the description..."
+  browser.goto 'https://apply.workable.com' + job_listing.css('a')[0]['href']
+  sleep 2
+  parse_page = Nokogiri::HTML(browser.html)
+  desc = parse_page.css('.job-preview-styles__description--2BkR3').text
+
+  puts "Saving job..."
+  job = Job.create!(
+    title: job_listing.css('.careers-jobs-list-styles__title--1cN5S').text.strip,
+    employer: 'Festicekt',
+    location: job_listings.css('.job-details-styles__details--11P0z').css('span')[1].text,
+    short_desc: '',
+    long_desc: desc,
+    link: 'https://apply.workable.com' + job_listing.css('a')[0]['href'],
+    source: "Festicket"
+    )
+end
 
 
